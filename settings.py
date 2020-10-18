1a70a4928e9a43742ba4e8e2fe65698d54dd0f8d
ggrantyoungg <73044181+ggrantyoungg@users.noreply.github.com>
2020-10-17 20:15:19 -0400

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,23 @@
+# define some colors (R, G, B)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARK = (40, 40, 40)
+GREY = (127, 127, 127)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+BLUE = (0, 0, 255)
+
+
+# game settings
+WIDTH = 1000
+HEIGHT = 300
+FPS = 30
+TITLE = "August Falls"
+BACKGROUND = DARK
+
+TILESIZE = 100
+GRIDWIDTH = WIDTH / TILESIZE
+GRIDHEIGHT = HEIGHT / TILESIZE
+
+FLOOR = 1
